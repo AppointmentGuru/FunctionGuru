c34d72f9f90f1eb6681dec3a87ac93120750f763
toast38coza <info@38.co.za>
2017-09-16 15:17:25 +0200

    Extract env vars. Add *.env to gitignore

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 from time import sleep
-import os
+import os, docker
 rabbit_user = os.environ.get('RABBITMQ_DEFAULT_USER')
 rabbit_pass = os.environ.get('RABBITMQ_DEFAULT_PASS')
 connection = 'amqp://{}:{}@broker:5672//'.format(rabbit_user, rabbit_pass)
@@ -13,6 +13,7 @@ def add(x, y):
 
 @app.task
 def run_in_container(image, command):
-    import docker, os
     client = docker.from_env()
+    # pull the image:
+    client.images.pull(image, auth_config={"username": os.environ(DOCKER_USERNAME), "password": "budry0kibri"}
     return client.containers.run(image, command)
